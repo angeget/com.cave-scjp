cea71c441e7a9f8e5ce4496388c1732eace858e0
Philip Hagenbruch <no-support>
2017-07-19 14:45:32 +0200

    20170719 100 Random Rects

--- src/main/java/com/philiphagenbruch/comcave/scjp/p20170719/AufgabeGeometrie.java
+++ src/main/java/com/philiphagenbruch/comcave/scjp/p20170719/AufgabeGeometrie.java
@@ -1,10 +1,18 @@
 package com.philiphagenbruch.comcave.scjp.p20170719;
 
+import java.util.Random;
+
 public class AufgabeGeometrie {
 
     public static void main(final String[] args) {
-        System.out.println(new Rect(3, 4).toString());
-        System.out.println(new Circle(5).toString());
+
+        final Random rand = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(new Rect(rand.nextInt(20) + 1, rand.nextInt(20) + 1).toString());
+        }
+
+        System.out.println("\n" + new Circle(5).toString());
     }
 
 }
