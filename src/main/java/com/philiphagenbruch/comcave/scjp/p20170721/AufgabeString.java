144301382dd67dedbc62a1db181fe0a355e94bb7
Philip Hagenbruch <no-support>
2017-07-21 11:56:58 +0200

    20170721 AufgabeString

---
+++ src/main/java/com/philiphagenbruch/comcave/scjp/p20170721/AufgabeString.java
@@ -0,0 +1,9 @@
+package com.philiphagenbruch.comcave.scjp.p20170721;
+
+public class AufgabeString {
+
+    public static void main(final String[] args) {
+        System.out.println(new StringBuilder("Java ist toll").reverse().toString());
+    }
+
+}
